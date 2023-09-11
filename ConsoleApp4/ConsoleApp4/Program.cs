add1144e3c4cb81410ab37b7fd71dee4b6ce87bf
gulsimag <85019072+gulsimag@users.noreply.github.com>
2023-09-11 21:13:43 +0300

    TOPLAMA VE IF KULLANIMI
    CONVERTTOINT32

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int sayi1,sayi2,secim,sonuc;
+            
+            for (int i=1;i<=5;i++)
+            {
+                Console.WriteLine("bir sayi giriniz:");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("bir sayi giriniz:");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("islemi secim 0-topla,1-cıkar,2-carp,3-bol:");
+                secim = Convert.ToInt32(Console.ReadLine());
+                if(secim == 0)
+                {
+                  sonuc=sayi1 + sayi2;
+
+                }
+               else if(secim==1)
+                {
+                    sonuc = sayi1 - sayi2;
+                }
+               else if (secim==2)
+                {
+                    sonuc = sayi1 * sayi2;
+                }
+                else if (secim==3)
+                {
+                    sonuc = sayi1 / sayi2;
+                }
+                
+
+                    Console.WriteLine("sonuc:" + sonuc);
+            }
+        }
+    }
+}
